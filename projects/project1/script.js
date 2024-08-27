923202cd955bbfe0f1866692b27f5403798ab0f2
Cesar <cerh_dev@outlook.com>
2024-08-27 16:25:06 -0600

    Add display message to project1

--- projects/project1/script.js
+++ projects/project1/script.js
@@ -1,1 +1,25 @@
 console.log("This is Project 1");
+
+const buttonSubmit = document.querySelector(".submitButton");
+const displayMessageDiv = document.querySelector(".displayMessage");
+
+// Function to check if the form is filled out
+function isFormValid() {
+  const inputs = document.querySelectorAll("input[required]");
+  for (let input of inputs) {
+    if (!input.value.trim()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Function to display success message
+function displayMessage() {
+  if (isFormValid()) {
+    displayMessageDiv.style.display = "flex";
+  }
+}
+
+// Add event listener to the button
+buttonSubmit.addEventListener("click", displayMessage);
